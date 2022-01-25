8f0a469336a53ab4576a7a772dae96ad67036e47
rajeshsanjeevi@itpeoplecorp.com <86312527+rajeshsanjeevi@users.noreply.github.com>
2022-01-24 19:36:53 -0500

    Update s3_public_bucket.tf

--- aws/aws_resources/s3_public_bucket.tf
+++ aws/aws_resources/s3_public_bucket.tf
@@ -1,6 +1,6 @@
 #creating an s3 bucket for HDS data extract for analytics node
 resource "aws_s3_bucket" "s3_bucket_logos_public" {
-  bucket = "${local.std_name}-${var.s3_bucket_name_logos_public}"
+  bucket = "${local.std_name}-${var.s3_bucket_name_logos}"
   acl    = "public-read"
   force_destroy = true
   versioning {
@@ -9,12 +9,12 @@ resource "aws_s3_bucket" "s3_bucket_logos_public" {
   tags = merge(
     local.tags,
     {
-      "Name" = "${local.std_name}-${var.s3_bucket_name_logos_public}"
+      "Name" = "${local.std_name}-${var.s3_bucket_name_logos}"
     },)
 }
 #S3 bucket policy for public s3 bucket
 resource "aws_s3_bucket_policy" "s3_bucket_policy_logos" {
-  bucket     = "${local.std_name}-${var.s3_bucket_name_logos_public}"
+  bucket     = "${local.std_name}-${var.s3_bucket_name_logos}"
   depends_on = [aws_s3_bucket.s3_bucket_logos_public]
   policy = jsonencode({
     "Version": "2012-10-17",
@@ -25,8 +25,8 @@ resource "aws_s3_bucket_policy" "s3_bucket_policy_logos" {
             "Principal": "*",
             "Action": "s3:GetObject",
             "Resource": [
-                "arn:aws:s3:::${local.std_name}-${var.s3_bucket_name_logos_public}",
-                "arn:aws:s3:::${local.std_name}-${var.s3_bucket_name_logos_public}/*",
+                "arn:aws:s3:::${local.std_name}-${var.s3_bucket_name_logos}",
+                "arn:aws:s3:::${local.std_name}-${var.s3_bucket_name_logos}/*",
 
             ]
         },
@@ -38,8 +38,8 @@ resource "aws_s3_bucket_policy" "s3_bucket_policy_logos" {
             },
             "Action": "*",
             "Resource": [
-                "arn:aws:s3:::${local.std_name}-${var.s3_bucket_name_logos_public}",
-                "arn:aws:s3:::${local.std_name}-${var.s3_bucket_name_logos_public}/*",
+                "arn:aws:s3:::${local.std_name}-${var.s3_bucket_name_logos}",
+                "arn:aws:s3:::${local.std_name}-${var.s3_bucket_name_logos}/*",
             ]
         },
       {
@@ -47,7 +47,7 @@ resource "aws_s3_bucket_policy" "s3_bucket_policy_logos" {
         Effect    = "Deny"
         Principal = "*"
         Action    = "s3:*"
-        Resource = "arn:aws:s3:::${local.std_name}-${var.s3_bucket_name_logos_public}/*",
+        Resource = "arn:aws:s3:::${local.std_name}-${var.s3_bucket_name_logos}/*",
         Condition = {
           Bool = {
             "aws:SecureTransport" = "false"
