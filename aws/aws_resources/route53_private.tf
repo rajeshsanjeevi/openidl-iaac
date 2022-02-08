c3b55183ee1501d483dfec555969d45bc5ac209d
rajesh.sanjeevi <rajesh.sanjeevi@itpeoplecorp.com>
2022-02-08 12:51:20 -0500

    updated dns

--- aws/aws_resources/route53_private.tf
+++ aws/aws_resources/route53_private.tf
@@ -1,7 +1,7 @@
 #creating private hosted zones for internal vpc dns resolution - databases and vault
 resource "aws_route53_zone" "aais_private_zones_internal" {
   name    = "internal.${var.domain_info.domain_name}"
-  comment = "Private hosted zones for dns resolution"
+  comment = "Private hosted zones for ${local.std_name}"
   vpc {
     vpc_id = module.aais_app_vpc.vpc_id
   }
@@ -18,7 +18,7 @@ resource "aws_route53_zone" "aais_private_zones_internal" {
 #creating private hosted zones for internal vpc dns resolution - others
 resource "aws_route53_zone" "aais_private_zones" {
   name    = "${var.aws_env}.${var.domain_info.sub_domain_name}.${var.domain_info.domain_name}"
-  comment = "Private hosted zones for dns resolution"
+  comment = "Private hosted zones for ${local.std_name}"
   vpc {
     vpc_id = module.aais_app_vpc.vpc_id
   }
