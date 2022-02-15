105b2e8ca61c268c8f45889f3069559d782d839a
rajesh.sanjeevi <rajesh.sanjeevi@itpeoplecorp.com>
2022-02-15 17:25:26 -0500

    test

--- aws/aws_resources/app_user-rolebased.tf
+++ aws/aws_resources/app_user-rolebased.tf
@@ -34,6 +34,31 @@ resource "aws_iam_user_policy" "openidl_apps_user_policy1" {
     ]
   })
 }
+resource "aws_iam_role" "openidl_apps_iam_role" {
+  name = "${local.std_name}-openidl-apps-role"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "sts:AssumeRole",
+                "sts:TagSession"
+            ],
+            "Principal": { "AWS": "arn:aws:iam::${var.aws_account_number}:user/${aws_iam_user.openidl_apps_user.name}"},
+            "Effect": "Allow",
+            "Condition": {
+                "StringEquals": {
+                    "sts:ExternalId": "apps-user"
+                }
+            }
+        }
+    ]
+  })
+  managed_policy_arns = [var.org_name == "aais" ? aws_iam_policy.openidl_aais_apps_role_policy[0].arn : aws_iam_policy.openidl_nonaais_apps_role_policy[0].arn]
+  tags = merge(local.tags, {Name = "${local.std_name}-openidl-apps-role", Cluster_type = "both"})
+  description = "The iam role that is used with OpenIDL apps to access AWS resources"
+  max_session_duration = 3600
+}
 #iam policy for openidl apps role
 resource "aws_iam_policy" "openidl_aais_apps_role_policy" {
   count = var.org_name == "aais" ? 1 : 0
@@ -154,56 +179,4 @@ resource "aws_iam_policy" "openidl_nonaais_apps_role_policy" {
   tags = merge(local.tags,
     { "Name" = "${local.std_name}-OpenIDL-APPs-Policy",
   "Cluster_type" = "both" })
-}
-resource "aws_iam_role" "openidl_nonaais_apps_iam_role" {
-  count = var.org_name == "aais" ? 0 : 1
-  name = "${local.std_name}-openidl-apps-role"
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "sts:AssumeRole",
-                "sts:TagSession"
-            ],
-            "Principal": { "AWS": "arn:aws:iam::${var.aws_account_number}:user/${aws_iam_user.openidl_apps_user[0].name}"},
-            "Effect": "Allow",
-            "Condition": {
-                "StringEquals": {
-                    "sts:ExternalId": "apps-user"
-                }
-            }
-        }
-    ]
-  })
-  managed_policy_arns = [aws_iam_policy.openidl_nonaais_apps_role_policy.arn]
-  tags = merge(local.tags, {Name = "${local.std_name}-openidl-apps-role", Cluster_type = "both"})
-  description = "The iam role that is used with OpenIDL apps to access AWS resources"
-  max_session_duration = 3600
-}
-resource "aws_iam_role" "openidl_aais_apps_iam_role" {
-  count = var.org_name == "aais" ? 1 : 0
-  name = "${local.std_name}-openidl-apps-role"
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "sts:AssumeRole",
-                "sts:TagSession"
-            ],
-            "Principal": { "AWS": "arn:aws:iam::${var.aws_account_number}:user/${aws_iam_user.openidl_apps_user[0].name}"},
-            "Effect": "Allow",
-            "Condition": {
-                "StringEquals": {
-                    "sts:ExternalId": "apps-user"
-                }
-            }
-        }
-    ]
-  })
-  managed_policy_arns = [aws_iam_policy.openidl_aais_apps_role_policy.arn]
-  tags = merge(local.tags, {Name = "${local.std_name}-openidl-apps-role", Cluster_type = "both"})
-  description = "The iam role that is used with OpenIDL apps to access AWS resources"
-  max_session_duration = 3600
 }
