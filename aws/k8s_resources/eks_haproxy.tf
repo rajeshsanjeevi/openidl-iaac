9fa5753a4d3d1c601034cdf7fbe8c85b746e1ce3
rajesh.sanjeevi <rajesh.sanjeevi@itpeoplecorp.com>
2022-02-17 01:13:41 -0500

    test

--- aws/k8s_resources/eks_haproxy.tf
+++ aws/k8s_resources/eks_haproxy.tf
@@ -1,17 +1,16 @@
 #setting up ha proxy in app cluster
-/*
 resource "helm_release" "app_haproxy" {
   depends_on = [data.aws_eks_cluster.app_eks_cluster, data.aws_eks_cluster_auth.app_eks_cluster_auth, kubernetes_config_map.app_config_map]
   provider = helm.app_cluster
   cleanup_on_fail = true
   name = "haproxy-ingress"
-  chart ="resources/haproxy-app-cluster-new"
+  chart ="resources/haproxy-app-cluster"
   timeout = 600
   force_update = true
   wait = true
   wait_for_jobs = true
-  values = ["${file("resources/haproxy-app-cluster-new/values.yaml")}"]
-}*/
+  values = ["${file("resources/haproxy-app-cluster/values.yaml")}"]
+}
 #setting up ha proxy in blk cluster
 resource "helm_release" "blk_haproxy" {
   depends_on = [data.aws_eks_cluster.blk_eks_cluster, data.aws_eks_cluster_auth.blk_eks_cluster_auth, kubernetes_config_map.blk_config_map]
@@ -25,18 +24,3 @@ resource "helm_release" "blk_haproxy" {
   wait_for_jobs = true
   values = ["${file("resources/haproxy-blk-cluster/values.yaml")}"]
 }
-#setting up ha proxy in app cluster
-/*
-resource "helm_release" "app_haproxy_new" {
-  depends_on = [data.aws_eks_cluster.app_eks_cluster, data.aws_eks_cluster_auth.app_eks_cluster_auth, kubernetes_config_map.app_config_map]
-  provider = helm.app_cluster
-  cleanup_on_fail = true
-  name = "haproxy-new1"
-  chart ="resources/haproxy-app-cluster-new"
-  timeout = 600
-  force_update = true
-  wait = true
-  wait_for_jobs = true
-  values = ["${file("resources/haproxy-app-cluster-new/values.yaml")}"]
-}
-*/
