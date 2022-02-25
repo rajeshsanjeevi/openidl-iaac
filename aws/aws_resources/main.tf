898d2d14aa9ac15f6fde306a98a449c48d3d2183
rajeshsanjeevi@itpeoplecorp.com <86312527+rajeshsanjeevi@users.noreply.github.com>
2022-02-25 00:51:32 -0500

    Update main.tf

--- aws/aws_resources/main.tf
+++ aws/aws_resources/main.tf
@@ -3,7 +3,7 @@ terraform {
   backend "remote" {
     hostname = "app.terraform.io"
     organization = "openidl-aais"
-    token = "XZqw2s5sXMUJbA.atlasv1.yuCQN8YovXBWWUhyBeMyiQW7uQjifocjEOIkvLz7xIQeeAzyYxA6FHZ8zQ7mnRmBRaU"
+    token = ""
     workspaces {
       name = "aais-dev-aws-resources"
     }
