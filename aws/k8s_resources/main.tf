a263ea4ea08b5d7798666439c41d23319ccf5a0e
rajeshsanjeevi@itpeoplecorp.com <86312527+rajeshsanjeevi@users.noreply.github.com>
2022-02-25 00:51:47 -0500

    Update main.tf

--- aws/k8s_resources/main.tf
+++ aws/k8s_resources/main.tf
@@ -7,7 +7,7 @@ terraform {
   backend "remote" {
     hostname = "app.terraform.io"
     organization = "openidl-aais"
-    token = "XZqw2s5sXMUJbA.atlasv1.yuCQN8YovXBWWUhyBeMyiQW7uQjifocjEOIkvLz7xIQeeAzyYxA6FHZ8zQ7mnRmBRaU"
+    token = ""
     workspaces {
       name = "aais-dev-k8s-resources"
     }
