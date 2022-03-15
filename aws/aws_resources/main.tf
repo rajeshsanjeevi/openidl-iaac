da5a7c56713925e80123e709e99ca2ae80ac38f0
rajesh.sanjeevi <rajesh.sanjeevi@itpeoplecorp.com>
2022-03-15 17:33:33 -0400

    updated copy having haproxy as ingresscontroller

--- aws/aws_resources/main.tf
+++ aws/aws_resources/main.tf
@@ -1,14 +1,3 @@
-#Uses Terraform Cloud/Enterprise as backend
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "openidl-aais"
-    token = ""
-    workspaces {
-      name = "aais-dev-aws-resources"
-    }
-  }
-}
 #Uses s3 as backend to manage terraform state files
 #terraform {
 #  backend "s3" {}
